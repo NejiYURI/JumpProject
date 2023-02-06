e0c6732681d597af736ae6547631f113950a2b39
NejiYURI <halo010354@gmail.com>
2023-02-06 16:38:26 -0700

    update new tile

---
+++ Assets/TileGenerator/TileData/Script/Tiles/LightTile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBasicTile", menuName = "TileObj/BasicTile")]
+public class NewBehaviourScript : TileData
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
